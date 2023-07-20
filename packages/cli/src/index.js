24f2e783cea1436a963bc03454ed735f1173a69f
sushobhit-lt <sushobhitd@lambdatest.com>
2023-07-20 15:24:59 +0530

    add web and static config commands

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,16 +1,16 @@
-import { Command,Option } from 'commander';
-// import { version } from '../package.json';
+import { Command, Option } from 'commander';
 import { capture } from './capture.js';
 import { logger } from '@smartui/logger';
 import { validateScreenshotConfig } from './validate.js';
 import packageJson from '../package.json' assert { type: 'json' };
+import { createWebConfig, createWebStaticConfig } from './config.js';
 
 const program = new Command();
 
 program
-    .name('smartui')
-    .description('CLI to help you to take screenshot SmartUI  on LambdaTest platform')
-    .addOption(new Option('--env <prod|stage>', 'Runtime environment option').choices(['prod', 'stage']));
+  .name('smartui')
+  .description('CLI to help you to take screenshot SmartUI  on LambdaTest platform')
+  .addOption(new Option('--env <prod|stage>', 'Runtime environment option').choices(['prod', 'stage']));
 
 program
   .command('capture <file>')
@@ -24,6 +24,26 @@ program
     log.info('config validation done');
     log.debug(options);
     capture(file, options, log);
+  });
+
+program.command('config:create-web')
+  .description('Create SmartUI Web config file')
+  .argument('[filepath]', 'Optional config filepath')
+  .action(async function (filepath, options) {
+    const log = await logger();
+    log.info('SmartUI Config CLI v' + packageJson.version);
+    log.info('\n');
+    createWebConfig(filepath, log);
+  });
+
+program.command('config:web-static')
+  .description('Create Web Static config file')
+  .argument('[filepath]', 'Optional config filepath')
+  .action(async function (filepath, options) {
+    const log = await logger();
+    log.info('SmartUI Config CLI v' + packageJson.version);
+    log.info('\n');
+    createWebStaticConfig(filepath, log);
   });
 
 program.parse();
