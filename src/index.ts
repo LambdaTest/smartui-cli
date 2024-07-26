382da92cd8968f6d312468d206c11c291be67623
parthlambdatest <parthkirsan@lambdatest.com>
2024-07-26 19:41:30 +0530

    final commit

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,16 @@ import pkgJSON from './../package.json'
     let log = logger;
 
     try {
-        let { data: { latestVersion, deprecated, additionalDescription, additionalDescriptionLatest } } = await client.checkUpdate(log);
+        let { data: { latestVersion, deprecated, additionalDescription, additionalDescriptionLatestVersion } } = await client.checkUpdate(log);
         log.info(`\nLambdaTest SmartUI CLI v${pkgJSON.version}`);
-        log.info(`${additionalDescription}`);
+        log.info(chalk.yellow(`${additionalDescription}`));
         if (deprecated){ 
             log.warn(`This version is deprecated. A new version ${latestVersion} is available!`);
-            log.warn(`${additionalDescriptionLatest}\n`);
+            log.warn(`${additionalDescriptionLatestVersion}\n`);
         }
         else if (pkgJSON.version !== latestVersion){ 
-            log.info(chalk.gray(`A new version ${latestVersion} is available!`));
-            log.info(`${additionalDescriptionLatest}\n`);
+            log.info(chalk.green(`A new version ${latestVersion} is available!`));
+            log.info(chalk.red(`${additionalDescriptionLatestVersion}\n`));
         }
         else log.info(chalk.gray('https://www.npmjs.com/package/@lambdatest/smartui-cli\n'));
     } catch (error) {
