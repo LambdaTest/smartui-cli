733a91c6045d3f4c041a4a03babe1a3383cdcd05
parthlambdatest <parthkirsan@lambdatest.com>
2024-08-03 00:36:22 +0530

    add new config option cli enable javascript - 5

--- src/lib/processSnapshot.ts
+++ src/lib/processSnapshot.ts
@@ -263,11 +263,6 @@ async function processSnapshot(snapshot: Snapshot, ctx: Context): Promise<Record
     }
 
     // add dom resources to cache
-
-    const domBase64 = Buffer.from(snapshot.dom.html);
-    
-    ctx.log.debug(`Cache entries: ${Object.keys(cache)}`);
-
     if (snapshot.dom.resources.length) {
         for (let resource of snapshot.dom.resources) {
             cache[resource.url] = {
