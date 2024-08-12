66a4b21931a9e75b46104fddd7fed8cae8ab2234
parthlambdatest <parthkirsan@lambdatest.com>
2024-08-12 15:36:55 +0530

    update scrollTime maximum value-2

--- src/lib/schemaValidation.ts
+++ src/lib/schemaValidation.ts
@@ -115,9 +115,9 @@ const ConfigSchema = {
         },
         scrollTime: {
             type: "number",
-            minimum: 8,
+            minimum: 1,
             maximum: 1000,
-            errorMessage: "Invalid config; scrollTime must be > 8 and <= 1000"
+            errorMessage: "Invalid config; scrollTime must be > 1 and <= 1000"
         },
         allowedHostnames: {
             type: "array",
