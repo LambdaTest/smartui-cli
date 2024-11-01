639ef45b11c7c1ba70d6033138a4b2488474bf83
parthlambdatest <parthkirsan@lambdatest.com>
2024-11-01 13:46:12 +0530

    code clean up

--- src/commander/uploadFigma.ts
+++ src/commander/uploadFigma.ts
@@ -23,8 +23,6 @@ command
     .action(async function(file, _, command) {
         let ctx: Context = ctxInit(command.optsWithGlobals());
 
-        const { fetchResults } = command.opts();
-
         if (!fs.existsSync(file)) {
             console.log(`Error: Figma Config file ${file} not found.`);
             return;
