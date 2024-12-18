ef95e19a695cd77291d218c5831adc4b6987dcc6
parthkirsan <parthkirsan@lambdatest.com>
2024-12-18 23:14:06 +0530

    refrain from deleting log file

--- src/tasks/finalizeBuild.ts
+++ src/tasks/finalizeBuild.ts
@@ -29,7 +29,6 @@ export default (ctx: Context): ListrTask<Context, ListrRendererFactory, ListrRen
                 ctx.log.debug(`Closed server`);
                 let resp = await ctx.client.getS3PreSignedURL(ctx);
                 await ctx.client.uploadLogs(ctx, resp.data.url);
-                unlinkSync(constants.LOG_FILE_PATH);
             } catch (error: any) {
                 ctx.log.debug(error);
             }
