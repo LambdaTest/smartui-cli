bd9008da08c7475245c10280fa063c8e1e4edcbe
shrinishLT <shrinishv@lambdatest.com>
2026-01-08 18:48:03 +0530

    remove changes

--- src/lib/ctx.ts
+++ src/lib/ctx.ts
@@ -186,8 +186,7 @@ export default (options: Record<string, string>): Context => {
             approvalThreshold: config.approvalThreshold,
             rejectionThreshold: config.rejectionThreshold,
             showRenderErrors: config.showRenderErrors ?? false,
-            customCSS: (config as any).customCSS,
-            mismatchOpacity: config.mismatchOpacity
+            customCSS: (config as any).customCSS
         },
         uploadFilePath: '',
         webStaticConfig: [],
